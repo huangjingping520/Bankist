3ae9b6b7377e1655826fe0fdcc10b7551141991e
huangjingping520 <6109119167@email.ncu.edu.cn>
2022-04-30 20:31:15 +0800

    :tada: feat: smooth scrolling

--- script.js
+++ script.js
@@ -57,5 +57,13 @@ const section1 = document.querySelector('#section--1')
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect()
 
-  window.scrollTo(s1coords.left, s1coords.top)
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset)
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+
+  section1.scrollIntoView({ behavior: 'smooth' })
 })
