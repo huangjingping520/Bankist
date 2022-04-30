ebe0116d5290b70b1293f8cc2f9a3f024e7ef004
huangjingping520 <6109119167@email.ncu.edu.cn>
2022-04-30 23:43:51 +0800

    feat: tabbed component

--- script.js
+++ script.js
@@ -62,6 +62,23 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 })
 
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab')
+
+  if (!clicked) return
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+  clicked.classList.add('operations__tab--active')
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+})
+
 // const message = document.createElement('div')
 // message.classList.add('cookie-message')
 // // message.textContent = 'We use cookied for improved functionality and analytics.'
