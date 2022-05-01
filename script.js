0e29360f2b380558946d03e3ea689bfa1f8b6dc5
huangjingping520 <6109119167@email.ncu.edu.cn>
2022-05-01 17:00:47 +0800

    feat: menu fade animation

--- script.js
+++ script.js
@@ -79,6 +79,27 @@ tabsContainer.addEventListener('click', function (e) {
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
 })
 
+const nav = document.querySelector('.nav')
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.nav').querySelector('img')
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this
+    })
+
+    logo.style.opacity = this
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5))
+
+nav.addEventListener('mouseout', handleHover.bind(1))
+
+
 // const message = document.createElement('div')
 // message.classList.add('cookie-message')
 // // message.textContent = 'We use cookied for improved functionality and analytics.'
