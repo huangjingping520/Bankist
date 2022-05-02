8e3da36e4b8b9de36ac350bdafad3d158ec83164
huangjingping520 <6109119167@email.ncu.edu.cn>
2022-05-02 21:26:30 +0800

    feat: slider

--- script.js
+++ script.js
@@ -168,10 +168,51 @@ const loadImg = function (entries, observer) {
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
-  threshold: 0
+  threshold: 0,
+  rootMargin: '200px'
 })
 
 imgTargets.forEach(img => imgObserver.observe(img))
+
+// Slider 
+const slides = document.querySelectorAll('.slide')
+const btnLeft = document.querySelector('.slider__btn--left')
+const btnRight = document.querySelector('.slider__btn--right')
+
+let curSlide = 0
+const maxSlide = slides.length
+
+// const slider = document.querySelector('.slider')
+// slider.style.transform = 'scale(0.4) translateX(-800px)'
+// slider.style.overflow = 'visible'
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`))
+}
+
+goToSlide(0)
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0
+  } else {
+    curSlide++
+  }
+
+  goToSlide(curSlide)
+}
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1
+  } else {
+    curSlide--
+  }
+  goToSlide(curSlide)
+}
+
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
 
 // const message = document.createElement('div')
 // message.classList.add('cookie-message')
